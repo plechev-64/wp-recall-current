21f7c21cd0909fe4f93d3e1d7d85451acce48bb6
Андрей <plechev.a@ya.ru>
2020-11-07 19:25:58 +0400

    16.24.28

--- classes/class-rcl-query-tables.php
+++ classes/class-rcl-query-tables.php
@@ -38,7 +38,9 @@ class Rcl_Posts_Query extends Rcl_Query {
 				'post_title',
 				'post_content',
 				'post_excerpt',
-				'post_parent'
+				'post_parent',
+				'post_mime_type',
+				'post_name'
 			)
 		);
 
