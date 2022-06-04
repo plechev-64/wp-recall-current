6c4a63e7e976305ca1f4efb8f7dc2e6c0a8183bc
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2022-06-04 16:23:33 +0400

    Прогнал под ВП темами. С ЛК закончил всё

--- add-on/theme-webx/templates/office.php
+++ add-on/theme-webx/templates/office.php
@@ -33,7 +33,7 @@
         <div class="webx-row">
             <div class="webx-col-md-3">
                 <div class="webx-area-menu">
-                    <a class="webx_phone_menu" href="#"><i class="rcli fa-bars"></i> <?php esc_html_e( 'Menu', 'wp-recall' ) ?></a>
+                    <a class="webx_phone_menu" href="#"><i class="rcli fa-bars"></i><span><?php esc_html_e( 'Menu', 'wp-recall' ) ?></span></a>
                     <div class="webx_phone_block">
 						<?php do_action( 'rcl_area_menu' ); ?>
                     </div>
